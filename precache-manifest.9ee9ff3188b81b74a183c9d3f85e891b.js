8890a64693807e103d7b23f7562c6469dfdb4eff
nuel-ikwuoma <emmanuelikwuoma7@gmail.com>
2020-09-19 10:46:26 +0100

    Updates

---
+++ precache-manifest.9ee9ff3188b81b74a183c9d3f85e891b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7aabbbca86ad5ee05cc",
+    "url": "/hacker-news-clone/static/css/main.cd2050d9.chunk.css"
+  },
+  {
+    "revision": "f7aabbbca86ad5ee05cc",
+    "url": "/hacker-news-clone/static/js/main.f7aabbbc.chunk.js"
+  },
+  {
+    "revision": "46f9c249b7b3f8766a1f",
+    "url": "/hacker-news-clone/static/js/1.46f9c249.chunk.js"
+  },
+  {
+    "revision": "a98814c031d2c8aeed84",
+    "url": "/hacker-news-clone/static/js/runtime~main.a98814c0.js"
+  },
+  {
+    "revision": "90f087e0cce8f34ed5ad7d79da9c0319",
+    "url": "/hacker-news-clone/index.html"
+  }
+];
